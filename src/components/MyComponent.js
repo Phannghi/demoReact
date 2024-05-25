ceea11af4cba28b6a6a486c55c7edfd2c05cc5fb
Phannghi <phanphucnghi9a3thcspt2002@gmail.com>
2024-05-25 23:44:35 +0700

    state, setState, DOM event, arrow function

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+    state = {
+        firstName: 'Nghi',
+        brand: 'Gucci',
+        car: 'Lambo',
+        age: 17
+    };
+    handleClick(event) {
+        console.log(this.state.brand);
+        console.log('>>random: ', Math.floor((Math.random() * 100) +1 ));
+        this.setState({
+            firstName : 'Bull',
+            age: this.state.age + 1
+        })
+    }
+    handleOnMouseOver(event){
+        //console.log(event);
+    }   
     render() {
         return (
-            <div> my Component
-                {Math.random()}
+            <div> 
+                My name is {this.state.firstName} and my age is {this.state.age}
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
             </div>
         );
     }
