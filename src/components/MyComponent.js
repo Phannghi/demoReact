a1706f6bf235d2fd55bc6da0e547d244b0bec8e8
Phannghi <phanphucnghi9a3thcspt2002@gmail.com>
2024-05-27 15:45:12 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import UserInfor from './UserInfor';
+import DisplayInfo from './DisplayInfo';
 
 class MyComponent extends React.Component {
     render() {
+        const myAge = 300;
         return (
             <div>
-                <UserInfor></UserInfor>
+                <UserInfor />
+                <br />
+                <DisplayInfo name="One piece" age={myAge} />
+                <br />
+                <DisplayInfo name="Joy boy" age="500" />
             </div>
         );
     }
