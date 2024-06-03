a712723185bd94b10abbb1515a4f73a2263860ac
Phannghi <phanphucnghi9a3thcspt2002@gmail.com>
2024-06-03 23:00:47 +0700

    route index

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Gucci Dior</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Gucci Dior</Navbar.Brand> */}
+                <NavLink to="/" className="navbar-brand">Gucci Dior</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -20,13 +21,13 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item >Log in</NavDropdown.Item>
+                            <NavDropdown.Item >
                                 Profile
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Setting</NavDropdown.Item>
+                            <NavDropdown.Item >Setting</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
+                            <NavDropdown.Item >
                                 Log out
                             </NavDropdown.Item>
                         </NavDropdown>
---
+++ src/components/home/Home.js
@@ -0,0 +1,8 @@
+const Home = (props) => {
+    return (
+        <div className="home-container">
+            Home Page container
+        </div>
+    )
+}
+export default Home;
