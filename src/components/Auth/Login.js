a070c6a396aa31d2524f76ec3466072df8f42a72
Phannghi <phanphucnghi9a3thcspt2002@gmail.com>
2024-06-15 16:37:39 +0700

    fix bug register

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -8,9 +8,26 @@ const Login = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handleLogin = async () => {
         // validate
-
+        const isValidEmail = validateEmail(email);
+        if (!isValidEmail) {
+            toast.error('Invalid email');
+            return;
+        }
+        if (!password) {
+            toast.error('Invalid password');
+            return;
+        }
         // submit api
         let data = await postLogin(email, password);
         console.log('check resp Login: ', data);
